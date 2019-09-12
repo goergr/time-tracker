4008d6c6556bb954bcdc6355b2bb3f4677be5d8a
goergr <51316226+goergr@users.noreply.github.com>
2019-09-12 16:49:44 +0200

    Update TrackerCoreConfig.java

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -1,12 +1,16 @@
+/**
+ * Created by Jason on 6/19/2015.
+ */
 package training.taylor.timetracker.core;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import training.taylor.timetracker.core.dao.TimeEntry;
 
-import java.util.ArrayList;
-import java.util.List;
+import training.taylor.timetracker.core.dao.TimeEntry;
 
 /**
  * Created by Jason on 6/19/2015.
@@ -15,6 +19,11 @@ import java.util.List;
 @ComponentScan("training.taylor.timetracker.core")
 public class TrackerCoreConfig {
 
+	/**
+	 * Bean.
+	 * 
+	 * @return entries
+	 */
     @Bean(name = "timesheet")
     public List<TimeEntry> timeEntries() {
         return new ArrayList<>();
