b8aff86e626a945db81f78a21fc90d07fa9838f6
goergr <51316226+goergr@users.noreply.github.com>
2019-09-12 16:49:03 +0200

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,20 +1,42 @@
+/**
+ * Created by Jason on 6/19/2015.
+ */
 package training.taylor.timetracker.core;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import training.taylor.timetracker.core.dao.TimeEntry;
+
+/**
+ * Created by Jason on 6/19/2015.
+ */
 @Component
 public class Tracker {
+	/**
+	 * time entries
+	 */
     @Autowired
     private List<TimeEntry> entries;
 
+	/**
+	 * add entry.
+	 * 
+	 * @param entry
+	 *            entry
+	 */
     public final void add(final TimeEntry entry) {
         entries.add(entry);
     }
 
+	/**
+	 * remove entry.
+	 * 
+	 * @param entry
+	 *            entry
+	 */
     public final void remove(final TimeEntry entry) {
 //        if (true) {
 //            entries.remove(entry);
@@ -23,10 +45,22 @@ public class Tracker {
         entries.remove(entry);
     }
 
+	/**
+	 * size.
+	 * 
+	 * @return size
+	 */
     public final int size() {
         return entries.size();
     }
 
+	/**
+	 * Getter.
+	 * 
+	 * @param index
+	 *            index
+	 * @return entry
+	 */
     public final TimeEntry get(final int index) {
         return entries.get(index);
     }
