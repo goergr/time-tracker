eb7bac0185c8c48fe6f43acf2aec2e20af145424
goergr <51316226+goergr@users.noreply.github.com>
2019-09-12 17:00:33 +0200

    Update TimeEntry.java

--- core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
+++ core/src/main/java/training/taylor/timetracker/core/dao/TimeEntry.java
@@ -12,68 +12,74 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 public class TimeEntry {
-	/**
-	 * description
-	 */
+    /**
+     * description
+     */
     private String description;
 
-	/**
-	 * rate
-	 */
+    /**
+     * rate
+     */
     private float rate;
 
-	/**
-	 * time
-	 */
+    /**
+     * time
+     */
     private int time;
 
-	/**
-	 * Getter
-	 * 
-	 * @return description
-	 */
+    /**
+     * Getter
+     *
+     * @return description
+     */
     final public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter
+     *
+     * @param value
+     *            description
+     */
     final public void setDescription(final String value) {
         this.description = value;
     }
 
-	/**
-	 * Getter
-	 * 
-	 * @return rate
-	 */
+    /**
+     * Getter
+     *
+     * @return rate
+     */
     final public float getRate() {
         return rate;
     }
 
-	/**
-	 * Setter
-	 * 
-	 * @param value
-	 *            rate
-	 */
+    /**
+     * Setter
+     *
+     * @param value
+     *            rate
+     */
     final public void setRate(final float value) {
         this.rate = value;
     }
 
-	/**
-	 * Getter
-	 * 
-	 * @return time
-	 */
+    /**
+     * Getter
+     *
+     * @return time
+     */
     final public int getTime() {
         return time;
     }
 
-	/**
-	 * Setter
-	 * 
-	 * @param value
-	 *            time
-	 */
+    /**
+     * Setter
+     *
+     * @param value
+     *            time
+     */
     final public void setTime(final int value) {
         this.time = value;
     }
